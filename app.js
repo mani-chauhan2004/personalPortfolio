0c3053e52e411b35f6e32f238ba9accef89579e4
mani-chauhan2004 <154721970+mani-chauhan2004@users.noreply.github.com>
2024-01-03 22:24:18 +0530

    Add files via uploadf

---
+++ app.js
@@ -0,0 +1,12 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#mainMat'),
+    smooth: true
+});
+
+var typed = new Typed(".types", {
+    strings: ["A 3D Enthusiast", "A Coder", "An Animator"],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true
+})
